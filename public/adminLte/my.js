4bf34e554391a4445c722ea1cc1463c1c411d24e
nartykbaev <artykbaev.nurdaulet@mail.ru>
2020-03-13 11:47:10 +0600

    end

--- public/adminLte/my.js
+++ public/adminLte/my.js
@@ -13,3 +13,28 @@ $('.sidebar-menu a.nav-link').each(function () {
     }
 });
 
+$('#reset-filter').click(function () {
+    $("#filter input[type=radio]").prop('checked',false);
+    return false;
+});
+
+$(".select2").select2({
+    placeholder: "Начните вводить наименование",
+    cache: true,
+    ajax:{
+        url: adminpath + "/product/related-product",
+        delay:250,
+        dataType: 'json',
+        data: function (params) {
+            return {
+                q:params.term,
+                page: params.page
+            };
+        },
+        processResults: function (data, params) {
+            return {
+                results: data.items,
+            };
+        },
+    },
+});
